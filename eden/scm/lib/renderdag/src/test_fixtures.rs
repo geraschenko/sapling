geraschenko/sapling

commit 658d95072d65571e7fc5c4ba16808c9a6268b5a4

renderdag: emit link_line for same-column named parents (#1293)
Summary:
Fixes https://github.com/facebook/sapling/issues/1292

The actual fix is the 3-line change to `eden/scm/lib/renderdag/src/render.rs`. Everything else is adding a new test fixture (`BASIC_DISCONNECTED`) and test (`basic_disconnected_min_row_height_1`) which demonstrates the issue.

Before / After (box_drawing, min_row_height=1) for new `BASIC_DISCONNECTED` fixture (`dag: "A B-C"`, heads `["A", "C"]`):
```
Before:
o  C
o  B
o  A

After:
o  C
│
o  B
o  A
```

Pull Request resolved: https://github.com/facebook/sapling/pull/1293

Reviewed By: giorgidze

Differential Revision: D106722405

Pulled By: quark-zju

fbshipit-source-id: 143c532a07a79d7bfda00abad1d6a294f1075c94

eden/scm/lib/renderdag/src/ascii.rs
@@ -76,6 +76,10 @@ where
             out.push('\n');
         }
 
+        if line.blank_line_before {
+            out.push('\n');
+        }
+
         // Render the nodeline
         let mut node_line = String::new();
         for entry in line.node_line.iter() {
@@ -236,6 +240,56 @@ mod tests {
             |
             o  B
             |
+            o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected() {
+        assert_eq!(
+            render(&test_fixtures::BASIC_DISCONNECTED),
+            r#"
+            o  D
+            |
+            o  C
+            
+            o  B
+            
+            o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected_min_row_height_1() {
+        let mut renderer = GraphRowRenderer::new()
+            .output()
+            .with_min_row_height(1)
+            .build_ascii();
+        assert_eq!(
+            render_string(&test_fixtures::BASIC_DISCONNECTED, &mut renderer),
+            r#"
+            o  D
+            o  C
+            
+            o  B
+            
+            o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected_min_row_height_1_staggered() {
+        let mut renderer = GraphRowRenderer::new()
+            .output()
+            .with_min_row_height(1)
+            .with_stagger_consecutive_disconnected_nodes(true)
+            .build_ascii();
+        assert_eq!(
+            render_string(&test_fixtures::BASIC_DISCONNECTED, &mut renderer),
+            r#"
+            o  D
+            o  C
+              o  B
             o  A"#
         );
     }

eden/scm/lib/renderdag/src/ascii_large.rs
@@ -78,6 +78,10 @@ where
             out.push('\n');
         }
 
+        if line.blank_line_before {
+            out.push('\n');
+        }
+
         // Render the nodeline
         let mut node_line = String::new();
         for (i, entry) in line.node_line.iter().enumerate() {
@@ -292,6 +296,24 @@ mod tests {
             o  B
             |
             |
+            o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected() {
+        assert_eq!(
+            render(&test_fixtures::BASIC_DISCONNECTED),
+            r#"
+            o  D
+            |
+            |
+            o  C
+            
+            
+            o  B
+            
+            
             o  A"#
         );
     }

eden/scm/lib/renderdag/src/box_drawing.rs
@@ -142,6 +142,10 @@ where
             out.push('\n');
         }
 
+        if line.blank_line_before {
+            out.push('\n');
+        }
+
         // Render the nodeline
         let mut node_line = String::new();
         for entry in line.node_line.iter() {
@@ -315,6 +319,56 @@ mod tests {
             │
             o  B
             │
+            o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected() {
+        assert_eq!(
+            render(&test_fixtures::BASIC_DISCONNECTED),
+            r#"
+            o  D
+            │
+            o  C
+            
+            o  B
+            
+            o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected_min_row_height_1() {
+        let mut renderer = GraphRowRenderer::new()
+            .output()
+            .with_min_row_height(1)
+            .build_box_drawing();
+        assert_eq!(
+            render_string(&test_fixtures::BASIC_DISCONNECTED, &mut renderer),
+            r#"
+            o  D
+            o  C
+            
+            o  B
+            
+            o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected_min_row_height_1_staggered() {
+        let mut renderer = GraphRowRenderer::new()
+            .output()
+            .with_min_row_height(1)
+            .with_stagger_consecutive_disconnected_nodes(true)
+            .build_box_drawing();
+        assert_eq!(
+            render_string(&test_fixtures::BASIC_DISCONNECTED, &mut renderer),
+            r#"
+            o  D
+            o  C
+              o  B
             o  A"#
         );
     }

eden/scm/lib/renderdag/src/output.rs
@@ -13,6 +13,8 @@ use super::box_drawing::BoxDrawingRenderer;
 use super::render::GraphRow;
 use super::render::Renderer;
 
+pub(crate) const DEFAULT_MIN_ROW_HEIGHT: usize = 2;
+
 pub(crate) struct OutputRendererOptions {
     pub(crate) min_row_height: usize,
 }
@@ -33,13 +35,21 @@ where
     pub fn new(inner: R) -> Self {
         OutputRendererBuilder {
             inner,
-            options: OutputRendererOptions { min_row_height: 2 },
+            options: OutputRendererOptions {
+                min_row_height: DEFAULT_MIN_ROW_HEIGHT,
+            },
             _phantom: PhantomData,
         }
     }
 
     pub fn with_min_row_height(mut self, min_row_height: usize) -> Self {
         self.options.min_row_height = min_row_height;
+        self.inner.set_min_row_height(min_row_height);
+        self
+    }
+
+    pub fn with_stagger_consecutive_disconnected_nodes(mut self, stagger: bool) -> Self {
+        self.inner.set_stagger_disconnected_nodes(stagger);
         self
     }
 

commit e8d9a3b5d7294ff95e39288d6edb60fe0219e27a

Fix issue introduced in 658d95072d6

eden/scm/lib/renderdag/src/ascii.rs
@@ -23,6 +23,8 @@ where
     inner: R,
     options: OutputRendererOptions,
     extra_pad_line: Option<String>,
+    extra_pad_line_occupied: Vec<bool>,
+    previous_graph_line_occupied: Vec<bool>,
     _phantom: PhantomData<N>,
 }
 
@@ -35,6 +37,8 @@ where
             inner,
             options,
             extra_pad_line: None,
+            extra_pad_line_occupied: Vec::new(),
+            previous_graph_line_occupied: Vec::new(),
             _phantom: PhantomData,
         }
     }
@@ -70,14 +74,30 @@ where
         let mut message_lines = pad_lines(line.message.lines(), self.options.min_row_height);
         let mut need_extra_pad_line = false;
 
+        let mut previous_graph_line_occupied = self.previous_graph_line_occupied.clone();
+
         // Render the previous extra pad line
         if let Some(extra_pad_line) = self.extra_pad_line.take() {
-            out.push_str(extra_pad_line.trim_end());
+            let extra_pad_line = extra_pad_line.trim_end();
+            out.push_str(extra_pad_line);
             out.push('\n');
+            previous_graph_line_occupied = std::mem::take(&mut self.extra_pad_line_occupied);
         }
 
-        if line.blank_line_before {
-            out.push('\n');
+        if line.needs_pre_node_separation {
+            if let Some(column) = line
+                .node_line
+                .iter()
+                .position(|entry| *entry == NodeLine::Node)
+            {
+                if previous_graph_line_occupied
+                    .get(column)
+                    .copied()
+                    .unwrap_or(false)
+                {
+                    out.push('\n');
+                }
+            }
         }
 
         // Render the nodeline
@@ -93,6 +113,11 @@ where
                 NodeLine::Blank => node_line.push_str("  "),
             }
         }
+        previous_graph_line_occupied = line
+            .node_line
+            .iter()
+            .map(|entry| *entry != NodeLine::Blank)
+            .collect();
         if let Some(msg) = message_lines.next() {
             node_line.push(' ');
             node_line.push_str(msg);
@@ -158,6 +183,7 @@ where
                     link_line.push(' ');
                 }
             }
+            previous_graph_line_occupied = link_row.iter().map(|entry| !entry.is_empty()).collect();
             if let Some(msg) = message_lines.next() {
                 link_line.push(' ');
                 link_line.push_str(msg);
@@ -182,6 +208,11 @@ where
                         });
                     }
                 }
+                previous_graph_line_occupied = term_row
+                    .iter()
+                    .zip(line.pad_lines.iter())
+                    .map(|(term, pad)| *term || *pad != PadLine::Blank)
+                    .collect();
                 if let Some(msg) = message_lines.next() {
                     term_line.push(' ');
                     term_line.push_str(msg);
@@ -193,6 +224,11 @@ where
         }
 
         let mut base_pad_line = String::new();
+        let base_pad_line_occupied: Vec<_> = line
+            .pad_lines
+            .iter()
+            .map(|entry| *entry != PadLine::Blank)
+            .collect();
         for entry in line.pad_lines.iter() {
             base_pad_line.push_str(match entry {
                 PadLine::Parent => "| ",
@@ -204,6 +240,7 @@ where
         // Render any pad lines
         for msg in message_lines {
             let mut pad_line = base_pad_line.clone();
+            previous_graph_line_occupied = base_pad_line_occupied.clone();
             pad_line.push(' ');
             pad_line.push_str(msg);
             out.push_str(pad_line.trim_end());
@@ -213,7 +250,10 @@ where
 
         if need_extra_pad_line {
             self.extra_pad_line = Some(base_pad_line);
+            self.extra_pad_line_occupied = base_pad_line_occupied;
         }
+
+        self.previous_graph_line_occupied = previous_graph_line_occupied;
 
         out
     }
@@ -274,6 +314,29 @@ mod tests {
             o  B
             
             o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected_min_row_height_1_with_extra_messages() {
+        let mut renderer = GraphRowRenderer::new()
+            .output()
+            .with_min_row_height(1)
+            .build_ascii();
+        assert_eq!(
+            render_string(
+                &test_fixtures::BASIC_DISCONNECTED_WITH_EXTRA_MESSAGES,
+                &mut renderer,
+            ),
+            r#"
+            o  D
+            |  extra
+            o  C
+               extra
+            o  B
+               extra
+            o  A
+               extra"#
         );
     }
 

eden/scm/lib/renderdag/src/ascii_large.rs
@@ -23,6 +23,8 @@ where
     inner: R,
     options: OutputRendererOptions,
     extra_pad_line: Option<String>,
+    extra_pad_line_occupied: Vec<bool>,
+    previous_graph_line_occupied: Vec<bool>,
     _phantom: PhantomData<N>,
 }
 
@@ -35,6 +37,8 @@ where
             inner,
             options,
             extra_pad_line: None,
+            extra_pad_line_occupied: Vec::new(),
+            previous_graph_line_occupied: Vec::new(),
             _phantom: PhantomData,
         }
     }
@@ -72,14 +76,30 @@ where
         let mut message_lines = pad_lines(line.message.lines(), self.options.min_row_height);
         let mut need_extra_pad_line = false;
 
+        let mut previous_graph_line_occupied = self.previous_graph_line_occupied.clone();
+
         // Render the previous extra pad line
         if let Some(extra_pad_line) = self.extra_pad_line.take() {
-            out.push_str(extra_pad_line.trim_end());
+            let extra_pad_line = extra_pad_line.trim_end();
+            out.push_str(extra_pad_line);
             out.push('\n');
+            previous_graph_line_occupied = std::mem::take(&mut self.extra_pad_line_occupied);
         }
 
-        if line.blank_line_before {
-            out.push('\n');
+        if line.needs_pre_node_separation {
+            if let Some(column) = line
+                .node_line
+                .iter()
+                .position(|entry| *entry == NodeLine::Node)
+            {
+                if previous_graph_line_occupied
+                    .get(column)
+                    .copied()
+                    .unwrap_or(false)
+                {
+                    out.push('\n');
+                }
+            }
         }
 
         // Render the nodeline
@@ -98,6 +118,11 @@ where
                 NodeLine::Blank => node_line.push_str(if i > 0 { "   " } else { "  " }),
             }
         }
+        previous_graph_line_occupied = line
+            .node_line
+            .iter()
+            .map(|entry| *entry != NodeLine::Blank)
+            .collect();
         if let Some(msg) = message_lines.next() {
             node_line.push(' ');
             node_line.push_str(msg);
@@ -186,6 +211,7 @@ where
                 top_link_line.push(' ');
                 top_link_line.push_str(msg);
             }
+            previous_graph_line_occupied = link_row.iter().map(|entry| !entry.is_empty()).collect();
             if let Some(msg) = message_lines.next() {
                 bot_link_line.push(' ');
                 bot_link_line.push_str(msg);
@@ -215,6 +241,11 @@ where
                         });
                     }
                 }
+                previous_graph_line_occupied = term_row
+                    .iter()
+                    .zip(line.pad_lines.iter())
+                    .map(|(term, pad)| *term || *pad != PadLine::Blank)
+                    .collect();
                 if let Some(msg) = message_lines.next() {
                     term_line.push(' ');
                     term_line.push_str(msg);
@@ -226,6 +257,11 @@ where
         }
 
         let mut base_pad_line = String::new();
+        let base_pad_line_occupied: Vec<_> = line
+            .pad_lines
+            .iter()
+            .map(|entry| *entry != PadLine::Blank)
+            .collect();
         for (i, entry) in line.pad_lines.iter().enumerate() {
             base_pad_line.push_str(match entry {
                 PadLine::Parent => {
@@ -255,6 +291,7 @@ where
         // Render any pad lines
         for msg in message_lines {
             let mut pad_line = base_pad_line.clone();
+            previous_graph_line_occupied = base_pad_line_occupied.clone();
             pad_line.push(' ');
             pad_line.push_str(msg);
             out.push_str(pad_line.trim_end());
@@ -264,7 +301,10 @@ where
 
         if need_extra_pad_line {
             self.extra_pad_line = Some(base_pad_line);
+            self.extra_pad_line_occupied = base_pad_line_occupied;
         }
+
+        self.previous_graph_line_occupied = previous_graph_line_occupied;
 
         out
     }

eden/scm/lib/renderdag/src/box_drawing.rs
@@ -76,6 +76,8 @@ where
     inner: R,
     options: OutputRendererOptions,
     extra_pad_line: Option<String>,
+    extra_pad_line_occupied: Vec<bool>,
+    previous_graph_line_occupied: Vec<bool>,
     glyphs: &'static [&'static str; glyph::COUNT],
     _phantom: PhantomData<N>,
 }
@@ -89,6 +91,8 @@ where
             inner,
             options,
             extra_pad_line: None,
+            extra_pad_line_occupied: Vec::new(),
+            previous_graph_line_occupied: Vec::new(),
             glyphs: &CURVED_GLYPHS,
             _phantom: PhantomData,
         }
@@ -136,14 +140,30 @@ where
         let mut message_lines = pad_lines(line.message.lines(), self.options.min_row_height);
         let mut need_extra_pad_line = false;
 
+        let mut previous_graph_line_occupied = self.previous_graph_line_occupied.clone();
+
         // Render the previous extra pad line
         if let Some(extra_pad_line) = self.extra_pad_line.take() {
-            out.push_str(extra_pad_line.trim_end());
+            let extra_pad_line = extra_pad_line.trim_end();
+            out.push_str(extra_pad_line);
             out.push('\n');
+            previous_graph_line_occupied = std::mem::take(&mut self.extra_pad_line_occupied);
         }
 
-        if line.blank_line_before {
-            out.push('\n');
+        if line.needs_pre_node_separation {
+            if let Some(column) = line
+                .node_line
+                .iter()
+                .position(|entry| *entry == NodeLine::Node)
+            {
+                if previous_graph_line_occupied
+                    .get(column)
+                    .copied()
+                    .unwrap_or(false)
+                {
+                    out.push('\n');
+                }
+            }
         }
 
         // Render the nodeline
@@ -159,6 +179,11 @@ where
                 NodeLine::Blank => node_line.push_str(glyphs[glyph::SPACE]),
             }
         }
+        previous_graph_line_occupied = line
+            .node_line
+            .iter()
+            .map(|entry| *entry != NodeLine::Blank)
+            .collect();
         if let Some(msg) = message_lines.next() {
             node_line.push(' ');
             node_line.push_str(msg);
@@ -244,6 +269,7 @@ where
                     link_line.push_str(glyphs[glyph::SPACE]);
                 }
             }
+            previous_graph_line_occupied = link_row.iter().map(|entry| !entry.is_empty()).collect();
             if let Some(msg) = message_lines.next() {
                 link_line.push(' ');
                 link_line.push_str(msg);
@@ -264,6 +290,11 @@ where
                         term_line.push_str(glyphs[line.pad_lines[i].to_glyph()]);
                     }
                 }
+                previous_graph_line_occupied = term_row
+                    .iter()
+                    .zip(line.pad_lines.iter())
+                    .map(|(term, pad)| *term || *pad != PadLine::Blank)
+                    .collect();
                 if let Some(msg) = message_lines.next() {
                     term_line.push(' ');
                     term_line.push_str(msg);
@@ -275,6 +306,11 @@ where
         }
 
         let mut base_pad_line = String::new();
+        let base_pad_line_occupied: Vec<_> = line
+            .pad_lines
+            .iter()
+            .map(|entry| *entry != PadLine::Blank)
+            .collect();
         for entry in line.pad_lines.iter() {
             base_pad_line.push_str(glyphs[entry.to_glyph()]);
         }
@@ -282,6 +318,7 @@ where
         // Render any pad lines
         for msg in message_lines {
             let mut pad_line = base_pad_line.clone();
+            previous_graph_line_occupied = base_pad_line_occupied.clone();
             pad_line.push(' ');
             pad_line.push_str(msg);
             out.push_str(pad_line.trim_end());
@@ -291,7 +328,10 @@ where
 
         if need_extra_pad_line {
             self.extra_pad_line = Some(base_pad_line);
+            self.extra_pad_line_occupied = base_pad_line_occupied;
         }
+
+        self.previous_graph_line_occupied = previous_graph_line_occupied;
 
         out
     }
@@ -353,6 +393,29 @@ mod tests {
             o  B
             
             o  A"#
+        );
+    }
+
+    #[test]
+    fn basic_disconnected_min_row_height_1_with_extra_messages() {
+        let mut renderer = GraphRowRenderer::new()
+            .output()
+            .with_min_row_height(1)
+            .build_box_drawing();
+        assert_eq!(
+            render_string(
+                &test_fixtures::BASIC_DISCONNECTED_WITH_EXTRA_MESSAGES,
+                &mut renderer,
+            ),
+            r#"
+            o  D
+            │  extra
+            o  C
+               extra
+            o  B
+               extra
+            o  A
+               extra"#
         );
     }
 

eden/scm/lib/renderdag/src/output.rs
@@ -44,7 +44,6 @@ where
 
     pub fn with_min_row_height(mut self, min_row_height: usize) -> Self {
         self.options.min_row_height = min_row_height;
-        self.inner.set_min_row_height(min_row_height);
         self
     }
 

eden/scm/lib/renderdag/src/render.rs
@@ -14,7 +14,6 @@ use serde::Serialize;
 
 use super::column::Column;
 use super::column::ColumnsExt;
-use super::output::DEFAULT_MIN_ROW_HEIGHT;
 use super::output::OutputRendererBuilder;
 
 pub trait Renderer<N> {
@@ -22,9 +21,6 @@ pub trait Renderer<N> {
 
     // Returns the width of the graph line, possibly including another node.
     fn width(&self, new_node: Option<&N>, new_parents: Option<&Vec<Ancestor<N>>>) -> u64;
-
-    // Set the minimum rendered row height.
-    fn set_min_row_height(&mut self, _min_row_height: usize) {}
 
     // Set whether disconnected consecutive nodes should be staggered into
     // different columns instead of separated by a blank line.
@@ -48,13 +44,9 @@ pub trait Renderer<N> {
 /// Converts a sequence of DAG node descriptions into rendered graph rows.
 pub struct GraphRowRenderer<N> {
     columns: Vec<Column<N>>,
-    // The remaining fields only have an effect when min_row_height is 1. With
-    // taller rows, the padding row already distinguishes connected same-column
-    // nodes from disconnected same-column nodes. But when there is no padding
-    // row, we must track what column the previous node is in so that either
-    // vertical or horizontal space can be added to indicate that the next node
-    // is not connected to the previous node.
-    min_row_height: usize,
+    // Track what column the previous node is in so output renderers can
+    // visually separate disconnected same-column nodes, either by adding
+    // vertical space or by staggering them into different columns.
     stagger_disconnected_nodes: bool,
     previous_node_column: Option<usize>,
 }
@@ -316,8 +308,10 @@ pub struct GraphRow<N> {
     /// The pad columns for this row.
     pub pad_lines: Vec<PadLine>,
 
-    /// True if a blank line should be rendered before this row.
-    pub blank_line_before: bool,
+    /// True if this row's node is disconnected from the previous node but
+    /// reuses its graph column. Output renderers should ensure that the two
+    /// nodes are visually separated in that graph column.
+    pub needs_pre_node_separation: bool,
 }
 
 impl<N> GraphRowRenderer<N>
@@ -328,7 +322,6 @@ where
     pub fn new() -> Self {
         GraphRowRenderer {
             columns: Vec::new(),
-            min_row_height: DEFAULT_MIN_ROW_HEIGHT,
             stagger_disconnected_nodes: false,
             previous_node_column: None,
         }
@@ -358,7 +351,7 @@ where
             // space for the node, then adding the new node would create
             // a new column.
             if self.columns.find(node).is_none() {
-                if self.min_row_height == 1 && self.stagger_disconnected_nodes {
+                if self.stagger_disconnected_nodes {
                     if let Some(previous_node_column) = self.previous_node_column {
                         if self.columns.get(previous_node_column) == Some(&Column::Empty) {
                             // Dense stagger mode cannot use the previous node's column for an
@@ -397,10 +390,6 @@ where
         width as u64
     }
 
-    fn set_min_row_height(&mut self, min_row_height: usize) {
-        self.min_row_height = min_row_height;
-    }
-
     fn set_stagger_disconnected_nodes(&mut self, stagger: bool) {
         self.stagger_disconnected_nodes = stagger;
     }
@@ -424,9 +413,9 @@ where
     ) -> GraphRow<N> {
         // Find a column for this node.
         let existing_column = self.columns.find(&node);
-        let mut blank_line_before = false;
+        let mut needs_pre_node_separation = false;
         let column = existing_column.unwrap_or_else(|| {
-            let column = if self.min_row_height == 1 && self.stagger_disconnected_nodes {
+            let column = if self.stagger_disconnected_nodes {
                 if let Some(index) = self.columns.iter().enumerate().find_map(|(index, column)| {
                     (*column == Column::Empty && Some(index) != self.previous_node_column)
                         .then_some(index)
@@ -443,9 +432,8 @@ where
                     .first_empty()
                     .unwrap_or_else(|| self.columns.new_empty())
             };
-            blank_line_before = self.min_row_height == 1
-                && !self.stagger_disconnected_nodes
-                && Some(column) == self.previous_node_column;
+            needs_pre_node_separation =
+                !self.stagger_disconnected_nodes && Some(column) == self.previous_node_column;
             column
         });
         self.columns[column] = Column::Empty;
@@ -607,7 +595,7 @@ where
             link_line,
             term_line,
             pad_lines,
-            blank_line_before,
+            needs_pre_node_separation,
         }
     }
 }

eden/scm/lib/renderdag/src/test_fixtures.rs
@@ -35,6 +35,20 @@ pub(crate) const BASIC_DISCONNECTED: TestFixture = TestFixture {
     missing: &[],
 };
 
+pub(crate) const BASIC_DISCONNECTED_WITH_EXTRA_MESSAGES: TestFixture = TestFixture {
+    dag: "A B C-D",
+    messages: &[
+        ("A", "extra"),
+        ("B", "extra"),
+        ("C", "extra"),
+        ("D", "extra"),
+    ],
+    heads: &["A", "B", "D"],
+    reserve: &[],
+    ancestors: &[],
+    missing: &[],
+};
+
 pub(crate) const BRANCHES_AND_MERGES: TestFixture = TestFixture {
     dag: r#"
                       T /---------------N--O---\           T